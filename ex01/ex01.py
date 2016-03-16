2456e64166cd68bce3168de98b67f4be749ce60f
CAD Client <CAD Client>
2016-03-16 20:16:33 +0900

    Added ex03.py

--- ex01/ex01.py
+++ ex01/ex01.py
@@ -1,25 +1,25 @@
-print "I will now count my chickens:" # account the chickin.
+#-*-coding:cp949
 
-print "Hens", 25 + 30 / 6 
-print "Roosters", 100 - 25 * 3 % 4 
+print "I will now count my chickens:" # 닭의 수를 세워봅시다..
 
-print "Now I will count the eggs:" 
+print "Hens", 25 + 30 / 6  # 암탉의 수
+print "Roosters", 100 - 25 * 3 % 4 # 수탉의 수
 
-print 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6 
+print "Now I will count the eggs:" # 달걀의 개수를 세워봅시다.
 
-print "Is it true that 3 + 2 < 5 - 7?"
+print 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6 # 달걀의 갯수
 
-print 3 + 2 < 5 - 7
+print "Is it true that 3 + 2 < 5 - 7?" # 맞습니까?
 
-print "What is 3 + 2?", 3 + 2 
-print "What is 5 - 7?", 5 - 7 
+print 3 + 2 < 5 - 7 # 식
 
-print "Oh, that's why it's False." 
+print "What is 3 + 2?", 3 + 2  # 3더하기 2는?
+print "What is 5 - 7?", 5 - 7 # 5 빼기 7은?
 
-print "How about some more." 
+print "Oh, that's why it's False." # 오, 왜 틀렸을까요?
 
-print "Is it greater?", 5 > -2 
-print "Is it greater or equal?", 5 >= -2 
-print "Is it less or equal?", 5 <= -2 
+print "How about some more." # 더 해봅시다.
 
-print "3 + 2 * 4"
+print "Is it greater?", 5 > -2 # 5가 -2보다 큰가요?
+print "Is it greater or equal?", 5 >= -2 # 5가 -2보다 크거나 같나요?
+print "Is it less or equal?", 5 <= -2 # 5가 -2보다 작거나 같나요?

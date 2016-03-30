97e4289d5e8408527d49631978bb7bdc7fe7685f
CAD Client <CAD Client>
2016-03-30 19:20:35 +0900

    Added ex10.py

---
+++ ex10/ex10.py
@@ -0,0 +1,19 @@
+#-*-coding:cp949
+
+tabby_cat = "\tI'm tabbed in."
+persian_cat = "I'm split\non a line."
+backslash_cat = "I'm \\ a \\ cat."
+
+fat_cat = ''' # 큰따옴표를 써도 되고 작은따옴표를 써도 됨.
+I'll do a list:
+\t* Cat food
+\t* Fishies
+\t* Catnip\n\t* Grass
+'''
+
+print tabby_cat
+print persian_cat
+print backslash_cat
+print fat_cat
+
+#Study Drill 4번 해보기

bcc8ab24c5ae9b0504ab279da914d20082b53818
CAD Client <CAD Client>
2016-03-30 18:58:28 +0900

    added ex05.py

---
+++ ex05/ex05.py
@@ -0,0 +1,20 @@
+#-*-coding:cp949
+
+my_name = 'ÀÌ±¤¼ö'
+my_age =  32 # not a lie
+my_height =  190 # centimeters
+my_weight = 78 # kilogram
+my_eyes = 'Black'
+my_teeth = 'White'
+my_hair = 'Black'
+
+print "Let's talk about %s." % my_name
+print "He's %d centimeters tall." % my_height
+print "He's %d kilogram heavy." % my_weight
+print "Actually that's not too heavy."
+print "He's got %s eyes and %s hair." % (my_eyes, my_hair)
+print "His teeth are usually %s depending on the coffee." % my_teeth
+
+# this line is tricky, try to get it exactly right
+print "If I add %d, %d, and %d I get %d." % (
+    my_age, my_height, my_weight, my_age + my_height + my_weight)

41ccbffbf779fe21fa1ec87a3f8dcad038743811
CAD Client <CAD Client>
2016-03-30 18:58:09 +0900

    added ex07.py

---
+++ ex07/ex07.py
@@ -0,0 +1,22 @@
+#-*-coding:cp949
+print "Mary had a little lamb." # Mary는 양을 키우고 있다.
+print "It's fleece was white as %s." % 'snow' # 그 양은 하얀양이다.
+print "And everywhere that Mary went."# 그리고 그양은 Mary가 가는 어디든지 따라온다.
+print "." * 10 # What'd that do? #문자열*숫자 = 문자열이 숫자만큼 반복
+
+end1 = "C"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "B"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r"
+
+# watcj thatcomma at the end. try remeoving it to see what happens
+print end1 + end2 + end3 + end4 + end5 + end6,
+print end7 + end8 + end9 + end10 + end11 + end12

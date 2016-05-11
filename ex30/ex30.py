f15a0eb1742fae080aaa733542794bd5497c3af7
CAD Client <CAD Client>
2016-05-11 18:17:02 +0900

    added ex30.py

---
+++ ex30/ex30.py
@@ -0,0 +1,25 @@
+# -*-coding:utf8
+# http://learnpythonthehardway.org/book/ex30.html
+
+people = 30
+cars = 40
+trucks = 15
+
+if cars > people:
+    print("We should take the cars.")
+elif cars < people:                         # elif는 갯수제한 없이 만들 수 있다.
+     print("we should not take the cars.")
+else:                                       # else는 1개 밖에 쓰지 못한다.if도 아니고 else도 아닐 때 
+    print("We can't decide.")
+
+if trucks > cars:
+    print("That's too many trucks.")
+elif trucks < cars:
+    print("Maybe we could take the trucks.")
+else:
+    print("We still can't decide.")
+
+if people > trucks:
+    print("Alright, let's just take the trucks.")
+else:
+    print("Fine, let's stay home then.")

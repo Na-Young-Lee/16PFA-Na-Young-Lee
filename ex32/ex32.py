a987e1df58824da432fc088238a919b7da224be2
CAD Client <CAD Client>
2016-05-18 18:15:21 +0900

    add ex32.py + loop기능

--- ex32/ex32.py
+++ ex32/ex32.py
@@ -5,7 +5,7 @@ the_count = [1, 2, 3, 4, 5]
 fruits = ['apples', 'oranges', 'pears', 'apricots']
 change = [1, 'pennies', 2, 'dimes', 3, 'quarters']
 
-# this first kind of for-loop goes through a list
+# this first kind of for-loop goes through a list #loop
 for number in the_count:
     print("This is count %d" % number)
 

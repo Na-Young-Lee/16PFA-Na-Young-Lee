8e983f6453107553466eb522bee8df9e0d194456
CAD Client <CAD Client>
2016-05-25 19:30:52 +0900

    ex38 수정

--- ex38/ex38.py
+++ ex38/ex38.py
@@ -1,3 +1,5 @@
+# coding: utf-8
+# http://learnpythonthehardway.org/book/ex39.html
 ten_things = "Apples Oranges Crows Telephone Light Sugar"
 
 print("Wait there are not 10 things in that list. Let's fix that.")
@@ -15,8 +17,8 @@ print("There we go: ", stuff)
 
 print("Let's do some things with stuff.")
 
-print(stuff[1])
-print(stuff[-1]) # whoa! fancy
-print(stuff.pop())
+print("stuff[1] = %s" % stuff[1])   # stuff[1] = oranges, stuff[0] = Apples
+print("stuff[-1] = %s" % stuff[-1]) # whoa! fancy
+print("stuff.pop() = %s" % stuff.pop())
 print(' '.join(stuff)) # what? cool!
 print('#'.join(stuff[3:5])) # super stellar!
